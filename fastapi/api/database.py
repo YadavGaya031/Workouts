b9885bac18307e16de1e85c72f4a2913a4c6363d
YadavGaya031 <singhgaya031@gmail.com>
2025-09-02 02:30:57 +0530

    minor issue fixing

--- fastapi/api/database.py
+++ fastapi/api/database.py
@@ -1,8 +1,11 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+import os
 
-SQL_ALCHEMY_DATABASE_URL = 'sqlite:///workout_app.db'
+load_dotenv()
+SQL_ALCHEMY_DATABASE_URL = os.getenv('SQL_ALCHEMY_DATABASE_URL')
 
 engine = create_engine(SQL_ALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
